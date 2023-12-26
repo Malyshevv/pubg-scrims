6007a8aaba42d55717c110ae5de6380500c0e96d
Malyshev R <malyshev.dev@ya.ru>
2023-12-26 10:41:55 +0300

    init

--- routes/web.php
+++ routes/web.php
@@ -21,18 +21,8 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', [NewsController::class, 'index'])->name('dashboard');
+    Route::get('/', [NewsController::class, 'index'])->name('dashboard');
 
     Route::get('/about-us', function () {
         return Inertia::render('AboutUs/AboutUs');
